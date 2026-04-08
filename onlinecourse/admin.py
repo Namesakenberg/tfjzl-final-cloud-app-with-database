ded9ebfc3291f746114562bf12ff1f08f5e0a314
Namesakenberg <siddharth.mane23@vit.edu>
2026-04-08 00:41:19 -0400

    Added admin for Question, Choice, Submission

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,18 +1,29 @@
 from django.contrib import admin
-# <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
-
-# <HINT> Register QuestionInline and ChoiceInline classes here
+# Import all models
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
 
+# Inline for Choice (inside Question)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+# Inline for Question (inside Course)
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
+
+
+# Existing Lesson inline
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
 
 
-# Register your models here.
+# Course Admin (add QuestionInline here)
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
+    inlines = [LessonInline, QuestionInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
@@ -22,9 +33,18 @@ class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
 
-# <HINT> Register Question and Choice models here
+#  Question Admin (add ChoiceInline here)
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'course', 'grade')
 
+
+# Register all models
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
